1a229fab69744e7d1fd68e900fff3be8832419f7
Guillaume DG <guillaume.dusseuxgicquel@gmail.com>
2020-04-16 17:11:46 +0200

    css home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import Button from '../components/Button';
 
+const ScreenText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 95vh;
+`;
+
 const Title = styled.h1`
   display: flex;
   justify-content: center;
@@ -10,18 +19,23 @@ const Title = styled.h1`
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  width: 25%;
+  text-align: center;
+  align-items: center;
+  margin: 0 auto;
 `;
 export default function Home() {
   const history = useHistory();
   return (
-    <div>
+    <ScreenText>
       <Title>Welcome</Title>
       <Container>
+        <h2>Enter your name</h2>
         <Button type="submit" onClick={() => history.push(`/quiz/category`)}>
           Start
         </Button>
       </Container>
-    </div>
+    </ScreenText>
   );
 }
